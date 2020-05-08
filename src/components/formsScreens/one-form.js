47cd2a0d1af46198261529c391648266b3f37150
sidahmed boutaraa <ahorob57@gmail.com>
2020-05-08 20:49:17 +0200

    colors selection setup

--- src/components/formsScreens/one-form.js
+++ src/components/formsScreens/one-form.js
@@ -1,27 +1,31 @@
-import React, { useState } from 'react';
-import '../../styles/froms/one/one-form.css'
+import React, { useState, useEffect } from 'react';
+import '../../styles/froms/one/one-form.css';
 
 
 function FormOne() {
 
     const [caseColor, setCaseColor] = useState("white");
-    const selectCaseColor = color => setCaseColor(color);
+    
+    // const [teethArray, setTeethArray] = useState([
+    //     [18, 17, 16, 26, 27, 28,],
+    //     [15, 14, 24, 25],
+    //     [13, 12, 11, 21, 22, 23],
+    //     [43, 42, 41, 31, 32, 33]
+    //     [45, 44, 34, 35],
+    //     [48, 47, 46, 36, 37, 38]
+    // ]);
 
-    const [teethArray, setTeehthArray] = useState([]);
-
-    for (let i = 0; i <= 32; i++) {
-        teethArray.push(
-            <div key={i}>
-                <span>{i}</span>
-                <span></span>
-                <span></span>
-            </div>
-        );
-    }
-
-    const setToothColor = (color, toothIndex) => {
-        console.log('get out of my way');
+    // for (let i = 0; i <= teethArray.length; i++) {
+    //     switch(i) {
+    //         case 0:
+    //     }
+    // }
+    
+    const setToothColor = e => {
+        e.preventDefault();
+        e.target.style.backgroundColor = caseColor;
     };
+    
 
     return (
         <div className="one">
@@ -50,19 +54,195 @@ function FormOne() {
                     <textarea />
                     <label>Date</label>
                     <input type="date" name="date" />
-                    <div className="teeth">
+                    <div className="teeth-colors">
                         <p>select a color that specify your case</p>
-                        <div className="colors">
+                        <div className="square-colors">
                             <span onClick={() => setCaseColor("red")}>case one</span>
                             <span onClick={() => setCaseColor("blue")}>case two</span>
                             <span onClick={() => setCaseColor("yellow")}>case three</span>
-                            <span onClick={() => setCaseColor("skyblue")}>case four</span>
-                            <span onClick={() => setCaseColor("navajowhite")}>case five</span>
-                            <span>case six</span>
-                            <span className="eraser">erase</span>
+                            <span onClick={() => setCaseColor("green")}>case four</span>
+                            <span onClick={() => setCaseColor("skyblue")}>case five</span>
+                            <span onClick={() => setCaseColor('navajowhite')}>case six</span>
+                            <span className="eraser" onClick={() => setCaseColor('white')}>erase</span>
                         </div>
                         <div className="dents">
-                            {teethArray}
+                            <div className="superior">
+                                <div className="tooth molar">
+                                    <div>
+                                        <span onClick={e => setToothColor(e)}>18</span>
+                                        <span onClick={e => setToothColor(e)}></span>
+                                        <span onClick={e => setToothColor(e)}></span>
+                                    </div>
+                                    <div>
+                                        <span onClick={e => setToothColor(e)}>17</span>
+                                        <span onClick={e => setToothColor(e)}></span>
+                                        <span onClick={e => setToothColor(e)}></span>
+                                    </div>
+                                    <div>
+                                        <span onClick={e => setToothColor(e)}>16</span>
+                                        <span onClick={e => setToothColor(e)}></span>
+                                        <span onClick={e => setToothColor(e)}></span>
+                                    </div>
+                                    <div>
+                                        <span onClick={e => setToothColor(e)}>26</span>
+                                        <span onClick={e => setToothColor(e)}></span>
+                                        <span onClick={e => setToothColor(e)}></span>
+                                    </div>
+                                    <div>
+                                        <span onClick={e => setToothColor(e)}>27</span>
+                                        <span onClick={e => setToothColor(e)}></span>
+                                        <span onClick={e => setToothColor(e)}></span>
+                                    </div>
+                                    <div>
+                                        <span onClick={e => setToothColor(e)}>28</span>
+                                        <span onClick={e => setToothColor(e)}></span>
+                                        <span onClick={e => setToothColor(e)}></span>
+                                    </div>                                  
+                                </div>
+                                <div className="tooth premolar">
+                                    <div>
+                                        <span onClick={e => setToothColor(e)}>15</span>
+                                        <span onClick={e => setToothColor(e)}></span>
+                                        <span onClick={e => setToothColor(e)}></span>
+                                    </div>
+                                    <div>
+                                        <span onClick={e => setToothColor(e)}>14</span>
+                                        <span onClick={e => setToothColor(e)}></span>
+                                        <span onClick={e => setToothColor(e)}></span>
+                                    </div>
+                                    <div>
+                                        <span onClick={e => setToothColor(e)}>24</span>
+                                        <span onClick={e => setToothColor(e)}></span>
+                                        <span onClick={e => setToothColor(e)}></span>
+                                    </div>
+                                    <div>
+                                        <span onClick={e => setToothColor(e)}>25</span>
+                                        <span onClick={e => setToothColor(e)}></span>
+                                        <span onClick={e => setToothColor(e)}></span>
+                                    </div>   
+                                </div>
+                                <div className="tooth anterior">
+                                    <div className="canine">
+                                        <span onClick={e => setToothColor(e)}>13</span>
+                                        <span onClick={e => setToothColor(e)}></span>
+                                        <span onClick={e => setToothColor(e)}></span>
+                                    </div>
+                                    <div>
+                                        <span onClick={e => setToothColor(e)}>12</span>
+                                        <span onClick={e => setToothColor(e)}></span>
+                                        <span onClick={e => setToothColor(e)}></span>
+                                    </div>
+                                    <div className="mid">
+                                        <span onClick={e => setToothColor(e)}>11</span>
+                                        <span onClick={e => setToothColor(e)}></span>
+                                        <span onClick={e => setToothColor(e)}></span>
+                                    </div>
+                                    <div className="mid">
+                                        <span onClick={e => setToothColor(e)}>21</span>
+                                        <span onClick={e => setToothColor(e)}></span>
+                                        <span onClick={e => setToothColor(e)}></span>
+                                    </div>
+                                    <div>
+                                        <span onClick={e => setToothColor(e)}>22</span>
+                                        <span onClick={e => setToothColor(e)}></span>
+                                        <span onClick={e => setToothColor(e)}></span>
+                                    </div>
+                                    <div className="canine">
+                                        <span onClick={e => setToothColor(e)}>23</span>
+                                        <span onClick={e => setToothColor(e)}></span>
+                                        <span onClick={e => setToothColor(e)}></span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="inferior">
+                                <div className="tooth anterior">
+                                    <div className="nin">
+                                        <span onClick={e => setToothColor(e)}>43</span>
+                                        <span onClick={e => setToothColor(e)}></span>
+                                        <span onClick={e => setToothColor(e)}></span>
+                                    </div>
+                                    <div>
+                                        <span onClick={e => setToothColor(e)}>42</span>
+                                        <span onClick={e => setToothColor(e)}></span>
+                                        <span onClick={e => setToothColor(e)}></span>
+                                    </div>
+                                    <div>
+                                        <span onClick={e => setToothColor(e)}>41</span>
+                                        <span onClick={e => setToothColor(e)}></span>
+                                        <span onClick={e => setToothColor(e)}></span>
+                                    </div>
+                                    <div>
+                                        <span onClick={e => setToothColor(e)}>31</span>
+                                        <span onClick={e => setToothColor(e)}></span>
+                                        <span onClick={e => setToothColor(e)}></span>
+                                    </div>
+                                    <div>
+                                        <span>32</span>
+                                        <span></span>
+                                        <span></span>
+                                    </div>
+                                    <div className="nin">
+                                        <span onClick={e => setToothColor(e)}>33</span>
+                                        <span onClick={e => setToothColor(e)}></span>
+                                        <span onClick={e => setToothColor(e)}></span>
+                                    </div>
+                                </div>
+                                <div className="tooth premolar">
+                                    <div>
+                                        <span onClick={e => setToothColor(e)}>45</span>
+                                        <span onClick={e => setToothColor(e)}></span>
+                                        <span onClick={e => setToothColor(e)}></span>
+                                    </div>
+                                    <div>
+                                        <span onClick={e => setToothColor(e)}>44</span>
+                                        <span onClick={e => setToothColor(e)}></span>
+                                        <span onClick={e => setToothColor(e)}></span>
+                                    </div>
+                                    <div>
+                                        <span onClick={e => setToothColor(e)}>34</span>
+                                        <span onClick={e => setToothColor(e)}></span>
+                                        <span onClick={e => setToothColor(e)}></span>
+                                    </div>
+                                    <div>
+                                        <span onClick={e => setToothColor(e)}>35</span>
+                                        <span onClick={e => setToothColor(e)}></span>
+                                        <span onClick={e => setToothColor(e)}></span>
+                                    </div> 
+                                </div>
+                                <div className="tooth molar">
+                                    <div>
+                                        <span onClick={e => setToothColor(e)}>48</span>
+                                        <span onClick={e => setToothColor(e)}></span>
+                                        <span onClick={e => setToothColor(e)}></span>
+                                    </div>
+                                    <div>
+                                        <span onClick={e => setToothColor(e)}>47</span>
+                                        <span onClick={e => setToothColor(e)}></span>
+                                        <span onClick={e => setToothColor(e)}></span>
+                                    </div>
+                                    <div>
+                                        <span onClick={e => setToothColor(e)}>46</span>
+                                        <span onClick={e => setToothColor(e)}></span>
+                                        <span onClick={e => setToothColor(e)}></span>
+                                    </div>
+                                    <div>
+                                        <span onClick={e => setToothColor(e)}>36</span>
+                                        <span onClick={e => setToothColor(e)}></span>
+                                        <span onClick={e => setToothColor(e)}></span>
+                                    </div>
+                                    <div>
+                                        <span onClick={e => setToothColor(e)}>37</span>
+                                        <span onClick={e => setToothColor(e)}></span>
+                                        <span onClick={e => setToothColor(e)}></span>
+                                    </div>
+                                    <div>
+                                        <span onClick={e => setToothColor(e)}>38</span>
+                                        <span onClick={e => setToothColor(e)}></span>
+                                        <span onClick={e => setToothColor(e)}></span>
+                                    </div>     
+                                </div>
+                                
+                            </div>
                         </div>
                     </div>
                     <button>submit the form</button>
